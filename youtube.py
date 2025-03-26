e4b6c3c1604607881474ba85803d268b92046414
chadmonkey <chadmonkey@yahoo.com>
2025-03-25 21:55:23 -0500

    trying youtube now

---
+++ youtube.py
@@ -0,0 +1,25 @@
+from googleapiclient.discovery import build
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.properties")
+
+api_key = config.get("API_KEYS", "YOUTUBE_API_KEY")
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Get trending videos in the US
+request = youtube.videos().list(
+    part="snippet,statistics",
+    chart="mostPopular",
+    regionCode="US",
+    maxResults=10
+)
+response = request.execute()
+
+# Print titles and views
+for video in response["items"]:
+    title = video["snippet"]["title"]
+    views = video["statistics"].get("viewCount", "N/A")
+    print(f"{title} - {views} views")
